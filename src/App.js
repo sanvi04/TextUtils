de3e28c5f4eb424374f288ce741679001a023e97
ZanV404 <sanviranjan808@gmail.com>
2025-06-25 18:59:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import { useState } from 'react';
+import Alert from './components/Alert';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const[mode, setMode] = useState('light');//whether mode is dark or light 
+    const[alert,setAlert]=useState(null);
+    const showAlert=(message, type)=>{
+        setAlert({
+            msg:message,
+            type:type
+        })
+        setTimeout(() => {
+            setAlert(null);
+            }, 1000);
+       
+    }
+    const toggleMode=()=>{
+        if(mode==='light'){
+           
+            setMode('dark');
+            document.body.style.backgroundColor = '#09100e';
+            showAlert("Dark mode has been enabled", "success");
+        }
+        else{
+            setMode('light');
+            document.body.style.backgroundColor = 'white';
+            showAlert("Light mode has been enabled", "success");
+        }   
+    }
+ return(
+  <>
+  <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}></Navbar>
+  <Alert alert={alert} />
+  <div className="container my-5">
+     <TextForm title="Enter the text below" mode={mode} showAlert={showAlert}/> 
+  </div>
+
+  
+  </>
+   
+ );
 }
 
 export default App;
+
+
---
+++ src/components/TextForm.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react'
+
+
+export default function TextForm(props) {
+  
+    
+    const [text, setText] = useState('');
+
+    const handleOnChange=(event)=>{
+      setText(event.target.value)
+    }
+
+    const handleUpClick=()=>{
+      let newText = text.toUpperCase();
+      setText(newText);
+      props.showAlert("Converted to upper case", "success");
+    }
+
+    const handleLoClick=()=>{
+      let newText = text.toLowerCase();
+      setText(newText);
+      props.showAlert("Converted to lower case", "success");
+    }
+
+    const handleClear=()=>{
+      
+      setText("");
+      props.showAlert("Text cleared", "success");
+    }
+
+    const handleCopy=()=>{
+      var text = document.getElementById("mybox");
+      text.select();
+      navigator.clipboard.writeText(text.value);
+      props.showAlert("Copied to clipboard", "success");
+    }
+
+    
+  return (
+    <>
+    <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
+      <div className="mb-3" >
+      <label htmlFor="mybox" className="form-label"><h2>{props.title}</h2></label>
+      <textarea className="form-control" id="mybox" rows="8" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black' }}></textarea>
+</div>
+<button className="btn btn-success mx-2" onClick={handleUpClick}>Convert to Upper case</button>
+<button className="btn btn-success mx-2" onClick={handleLoClick}>Convert to Lower case</button>
+<button className="btn btn-success mx-2" onClick={handleClear}>Clear Text</button>
+<button className="btn btn-success mx-2" onClick={handleCopy}>Copy Text</button>
+
+    </div>
+    <div className="container my-5" style={{color: props.mode==='dark'?'white':'black'}}>
+      <h2> Your text summary</h2>
+      <p> {text.split(" ").length-1} words, {text.length} characters</p>
+      <p>{(text.split(" ").length-1)*0.008} minutes to read</p>
+      <h2>Preview</h2>
+      <p>{text.length>0?text:"Enter text to preview here"}</p>
+    </div>
+    </>
+  )
+}
