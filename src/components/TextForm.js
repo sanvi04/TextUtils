4babe60fef85fc47188385d6c398789172ae3342
ZanV404 <sanviranjan808@gmail.com>
2025-07-08 23:40:34 +0530

    undoing changes

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -43,15 +43,15 @@ export default function TextForm(props) {
       <label htmlFor="mybox" className="form-label"><h2>{props.title}</h2></label>
       <textarea className="form-control" id="mybox" rows="8" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black' }}></textarea>
 </div>
-<button className="btn btn-success mx-3 my-1" onClick={handleUpClick}>Convert to Upper case</button>
-<button className="btn btn-success mx-3 my-1" onClick={handleLoClick}>Convert to Lower case</button>
-<button className="btn btn-success mx-3 my-1" onClick={handleClear}>Clear Text</button>
-<button className="btn btn-success mx-3 my-1" onClick={handleCopy}>Copy Text</button>
+<button className="btn btn-success mx-3 " onClick={handleUpClick}>Convert to Upper case</button>
+<button className="btn btn-success mx-3 " onClick={handleLoClick}>Convert to Lower case</button>
+<button className="btn btn-success mx-3 " onClick={handleClear}>Clear Text</button>
+<button className="btn btn-success mx-3 " onClick={handleCopy}>Copy Text</button>
 
     </div>
     <div className="container my-5" style={{color: props.mode==='dark'?'white':'black'}}>
       <h2> Your text summary</h2>
-      <p> {text.split(" ").filter((element)=>{return element.length!==0}).length} words, {text.length} characters</p>
+      <p> {text.split(" ").length-1} words, {text.length} characters</p>
       <p>{(text.split(" ").length-1)*0.008} minutes to read</p>
       <h2>Preview</h2>
       <p>{text.length>0?text:"Enter text to preview here"}</p>
